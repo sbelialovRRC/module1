c4998ee166b81a676c20157f2fa291cff7728df6
Sergei Belialov <32887986+godszerg86@users.noreply.github.com>
2025-09-02 13:13:32 -0500

    tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -17,3 +17,17 @@ describe("GET /", () => {
         expect(response.text).toBe("Hello, world!");
     });
 });
+
+describe("GET /api/v1/health", () => {
+    it("should return server health status", async () => {
+        // create GET request to health endpoint
+        const response: Response = await request(app).get("/api/v1/health");
+
+        // assert response status OK and health object to have specified properties
+        expect(response.status).toBe(200);
+        expect(response.body.status).toBe("OK");
+        expect(response.body).toHaveProperty("uptime");
+        expect(response.body).toHaveProperty("timestamp");
+        expect(response.body).toHaveProperty("version");
+    });
+});
